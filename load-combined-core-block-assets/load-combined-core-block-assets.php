dc2dd7fc3ebe2ad972dc71b445cfb7993491d226
Sean Murphy <sean@iamseanmurphy.com>
2026-02-06 06:29:33 +0000

    1.0.0

---
+++ load-combined-core-block-assets/load-combined-core-block-assets.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Load Combined Core Block Assets Plugin for WordPress
+ *
+ * @package   LoadCombinedCoreBlockAssets
+ * @author    Weston Ruter
+ * @license   GPL-2.0-or-later
+ * @copyright Copyleft 2025, Weston Ruter
+ *
+ * @wordpress-plugin
+ * Plugin Name: Load Combined Core Block Assets
+ * Plugin URI: https://github.com/westonruter/load-combined-core-block-assets
+ * Description: Temporary workaround for sites experiencing issues with WordPress 6.9's new ability to <a href="https://make.wordpress.org/core/2025/11/18/wordpress-6-9-frontend-performance-field-guide/#load-block-styles-on-demand-in-classic-themes">load block styles on demand in classic themes</a>.
+ * Requires at least: 6.9
+ * Requires PHP: 7.2
+ * Version: 1.0.0
+ * Author: Weston Ruter
+ * Author URI: https://weston.ruter.net/
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * GitHub Plugin URI: https://github.com/westonruter/load-combined-core-block-assets
+ * Primary Branch: main
+ */
+
+namespace LoadCombinedCoreBlockAssets;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // @codeCoverageIgnore
+}
+
+/**
+ * Plugin version.
+ *
+ * @since 0.1.0
+ * @var string
+ */
+const VERSION = '1.0.0';
+
+/**
+ * Inits plugin.
+ *
+ * @since 0.1.0
+ *
+ * @return void
+ */
+function init() { // phpcs:ignore SlevomatCodingStandard.TypeHints.ReturnTypeHint
+	if ( wp_is_block_theme() ) {
+		return;
+	}
+	add_filter(
+		'should_load_separate_core_block_assets',
+		__NAMESPACE__ . '\filter_should_load_separate_core_block_assets',
+		PHP_INT_MAX
+	);
+}
+
+add_action( 'after_setup_theme', __NAMESPACE__ . '\init' );
+
+/**
+ * Filters should_load_separate_core_block_assets.
+ *
+ * @since 0.1.0
+ *
+ * @return bool Whether should_load_separate_core_block_assets is enabled.
+ */
+function filter_should_load_separate_core_block_assets(): bool {
+	if ( ! isset( $_GET['should_load_separate_core_block_assets'] ) || ! is_string( $_GET['should_load_separate_core_block_assets'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+		return false;
+	}
+
+	return rest_sanitize_boolean( $_GET['should_load_separate_core_block_assets'] ); // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+}
+
+/**
+ * Filters the HTML link tag of an enqueued style.
+ *
+ * @since 0.1.0
+ *
+ * @param string|mixed $tag    The link tag for the enqueued style.
+ * @param string       $handle The style's registered handle.
+ */
+function filter_style_loader_tag( $tag, string $handle ): string {
+	if ( ! is_string( $tag ) ) {
+		return '';
+	}
+	if ( 'wp-block-library' === $handle && ! wp_should_load_separate_core_block_assets() ) {
+		$comment = sprintf(
+			/* translators: 1: wp-block-library, 2: should_load_separate_core_block_assets, 3: ?should_load_separate_core_block_assets=true */
+			__( 'Note: This combined block library stylesheet (%1$s) is used instead of loading separate core block styles because the %2$s filter is returning false. Try loading the URL with %3$s to restore being able to load block styles on demand and see if there are any issues remaining.', 'load-combined-core-block-assets' ),
+			'wp-block-library',
+			'should_load_separate_core_block_assets',
+			'?should_load_separate_core_block_assets=true'
+		);
+
+		$tag = sprintf( "\n<!-- %s -->\n%s", esc_html( $comment ), $tag );
+	}
+	return $tag;
+}
+
+add_filter( 'style_loader_tag', __NAMESPACE__ . '\filter_style_loader_tag', 10, 2 );
---
+++ load-combined-core-block-assets/readme.txt
@@ -0,0 +1,48 @@
+=== Load Combined Core Block Assets ===
+
+Contributors: westonruter
+Tags: performance
+Tested up to: 6.9
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+
+Temporary workaround for sites experiencing issues with WordPress 6.9's new ability to load block styles on demand in classic themes.
+
+== Description ==
+
+This is a temporary workaround for sites experiencing any issues with WordPress 6.9's new ability to [load block styles on demand in classic themes](https://make.wordpress.org/core/2025/11/18/wordpress-6-9-frontend-performance-field-guide/#load-block-styles-on-demand-in-classic-themes), for example [#64354](https://core.trac.wordpress.org/ticket/64354).
+
+If running a classic theme (i.e. not a block theme), it filters `should_load_separate_core_block_assets` to be `false`. This has the effect of reverting a change introduced in 6.9 where classic themes now load separate core block assets on demand by default.
+
+Note that this plugin should be considered temporary until any issues are resolved in 6.9.1. At any time, you can test whether any issues remain by adding `?should_load_separate_core_block_assets=true` to any frontend URL; this restores the default behavior in WP 6.9. 
+
+This workaround is temporary because there are performance benefits to loading separate core block assets. They can be loaded on demand just when they are used, as opposed to loading the large single combined `wp-block-library` stylesheet. Loading separate block styles on demand reduces the amount of CSS which should improve page load time.
+
+== Installation ==
+
+= Automatic =
+
+1. Visit **Plugins > Add New** in the WordPress Admin.
+2. Search for **Load Combined Core Block Assets**.
+3. Install and activate the **Load Combined Core Block Assets** plugin.
+
+You may also install and update via [Git Updater](https://git-updater.com/) using the [plugin's GitHub URL](https://github.com/westonruter/load-combined-core-block-assets).
+
+= Manual =
+
+1. Download the plugin ZIP either [from WordPress.org](https://downloads.wordpress.org/plugin/load-combined-core-block-assets.zip) or [from GitHub](https://github.com/westonruter/load-combined-core-block-assets/archive/refs/heads/main.zip). Alternatively, if you have a local clone of the repo, run `npm run plugin-zip`.
+2. Visit **Plugins > Add New Plugin** in the WordPress Admin.
+3. Click **Upload Plugin**.
+4. Select the `load-combined-core-block-assets.zip` file on your system from step 1 and click **Install Now**.
+5. Click the **Activate Plugin** button.
+
+== Changelog ==
+
+= 1.0.0 =
+
+* Escape translated message in HTML comment for extra hardening.
+
+= 0.1.0 =
+
+* Initial release.
